853fe5c77bd9f48daa0f4158cc3eed9f745b47b3
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2023-11-21 20:52:08 +0800

    Adding HL Test for STEMI and NSTEMI

--- StructuringFunction.R
+++ StructuringFunction.R
@@ -20,3 +20,41 @@ StructuringFunction <- function(dataset, cat_nb_features_names, cat_features_nam
   
   return(dataset)
 }
+
+StemiStructuringFunction <- function(dataset, cat_nb_features_names, cat_features_names, cont_features_names) {
+  # Change the types of variables in dataset
+  #categorical non binary
+  dataset[,cat_nb_features_names] <- lapply(dataset[,cat_nb_features_names], as.factor)
+  
+  #categorical
+  dataset[,cat_features_names] <- ifelse(dataset[,cat_features_names] == 1, 1, 0)
+  dataset[,cat_features_names] <- lapply(dataset[,cat_features_names], as.factor)
+  
+  #continuos & ordinal
+  zscore_scale <- preProcess(dataset[,cont_features_names],method=c("center", "scale"))
+  dataset[,cont_features_names] <- predict(zscore_scale, dataset[,cont_features_names])
+  
+  # Changing ptoutcome to binary
+  dataset$timiscorestemi <- as.factor(dataset$timiscorestemi)
+  
+  return(dataset)
+}
+
+NStemiStructuringFunction <- function(dataset, cat_nb_features_names, cat_features_names, cont_features_names) {
+  # Change the types of variables in dataset
+  #categorical non binary
+  dataset[,cat_nb_features_names] <- lapply(dataset[,cat_nb_features_names], as.factor)
+  
+  #categorical
+  dataset[,cat_features_names] <- ifelse(dataset[,cat_features_names] == 1, 1, 0)
+  dataset[,cat_features_names] <- lapply(dataset[,cat_features_names], as.factor)
+  
+  #continuos & ordinal
+  zscore_scale <- preProcess(dataset[,cont_features_names],method=c("center", "scale"))
+  dataset[,cont_features_names] <- predict(zscore_scale, dataset[,cont_features_names])
+  
+  # Changing ptoutcome to binary
+  dataset$timiscorenstemi <- as.factor(dataset$timiscorenstemi)
+  
+  return(dataset)
+}
