90326d03f72d98ba923cafb4a2ca9b86b90bfe48
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2023-11-21 15:10:35 +0800

    Changing Dataset

--- 03_Platt_Scaling.R
+++ 03_Platt_Scaling.R
@@ -26,7 +26,7 @@ library(pROC)
 
 #### Loading and Change Types of All Datasets ####
 # Include [-1] to remove "X" column of row number
-testing_ds <- read.csv('./dataset/processed/testing_ds.csv', header = TRUE)[-1]
+# testing_ds <- read.csv('./dataset/processed/testing_ds.csv', header = TRUE)[-1]
 calibration_ds <- read.csv('./dataset/processed/calibration_ds.csv', header = TRUE)[-1]
 validation_ds <- read.csv('./dataset/processed/validation_ds.csv', header = TRUE)[-1]
 
@@ -40,12 +40,17 @@ cat_nb_features_names <- all_features$VarNames[which(all_features$Cat_Cont_Ord =
 cat_features_names<- all_features$VarNames[which(all_features$Cat_Cont_Ord == "categorical_binary")] 
 cont_features_names <- all_features$VarNames[which(all_features$Cat_Cont_Ord %in% c("continuous", "ordinal"))]
 
+# get selected features in DS
+#testing_ds <- testing_ds[,c(all_features_names,"ptoutcome")]
+calibration_ds <- calibration_ds[,c(all_features_names,"ptoutcome")]
+validation_ds <- validation_ds[,c(all_features_names,"ptoutcome")]
+
 # Change the types of each variables
+# testing_ds <- StructuringFunction(dataset = testing_ds, cat_nb_features_names, cat_features_names, cont_features_names)
 calibration_ds <- StructuringFunction(dataset = calibration_ds, cat_nb_features_names, cat_features_names, cont_features_names)
 validation_ds <- StructuringFunction(dataset = validation_ds, cat_nb_features_names, cat_features_names, cont_features_names)
-testing_ds <- StructuringFunction(dataset = testing_ds, cat_nb_features_names, cat_features_names, cont_features_names)
 
-X_test_testing <- testing_ds[, -which(names(testing_ds) == 'ptoutcome')]
+# X_test_testing <- testing_ds[, -which(names(testing_ds) == 'ptoutcome')]
 X_test_calib <- calibration_ds[, -which(names(calibration_ds) == 'ptoutcome')]
 X_test_valid <- validation_ds[, -which(names(validation_ds) == 'ptoutcome')]
 
@@ -56,20 +61,15 @@ model <- readRDS('./models/3_modelAll_rf20_dm.rds')
 model <- model$RF
 
 # Predicting for calibration and validation dataset
-pred_prob_testing <- as.data.frame(predict(model, X_test_testing, type= 'prob')$Death)
+# pred_prob_testing <- as.data.frame(predict(model, X_test_testing, type= 'prob')$Death)
 pred_prob_calib <- as.data.frame(predict(model, X_test_calib, type= 'prob')$Death)
 pred_prob_valid <- as.data.frame(predict(model, X_test_valid, type= 'prob')$Death)
 
-pred_prob_testing$ptoutcome <- as.numeric(testing_ds$ptoutcome== 0)
-pred_prob_calib$ptoutcome <- as.numeric(calibration_ds$ptoutcome== 0)
-pred_prob_valid$ptoutcome <- as.numeric(validation_ds$ptoutcome == 0)
-
-#### HL Test for testing dataset ####
-hoslem.test(pred_prob_testing$ptoutcome,
-            pred_prob_testing$`predict(model, X_test_testing, type = "prob")$Death`, 
-            g=10)
-
-#-squared = 6920.8, df = 8, p-value < 2.2e-16
+# pred_prob_testing$ptoutcome <- as.numeric(testing_ds$ptoutcome== 0)
+pred_prob_calib$ptoutcome <- as.numeric(calibration_ds$ptoutcome)
+pred_prob_calib$ptoutcome <- as.numeric(pred_prob_calib$ptoutcome-1)
+pred_prob_valid$ptoutcome <- as.numeric(validation_ds$ptoutcome)
+pred_prob_valid$ptoutcome <- as.numeric(pred_prob_valid$ptoutcome-1)
 
 
 #### Platt Scaling ####
@@ -99,5 +99,9 @@ cal_auc$auc
 pred_risk <- res$cal.probs
 ptoutcome <- res$responses
 
-hoslem.test(ptoutcome,pred_risk, g=10)
+hoslem.test(pred_prob_valid$ptoutcome,pred_risk, g=10)
 
+#	Hosmer and Lemeshow goodness of fit (GOF) test
+
+# data:  pred_prob_valid$ptoutcome, pred_risk
+# X-squared = 10.349, df = 8, p-value = 0.2414
