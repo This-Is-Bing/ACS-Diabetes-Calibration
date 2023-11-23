cbaeed439be86d0ff7e4a2886ba852b9aa3929da
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2023-11-23 13:25:43 +0800

    Adding searchThreshold function

--- Functions.R
+++ Functions.R
@@ -159,3 +159,106 @@ Evaluation <- function(label, predicted, rowname){
                        row.names = rowname )
   return (result)
 }
+
+#### Evaluation ####
+Evaluation <- function(label, predicted, threshold, rowname){
+  
+  label <- as.numeric(label)-1
+  predicted <- as.numeric(predicted)
+  
+  roc_curve <- roc(label, predicted)
+  
+  # Get the coordinates of the point on the ROC curve that is closest to the top-left corner
+  coords_best <- coords(roc_curve, "best", best.method = "closest.topleft")
+  
+  # Access the threshold value from the coordinates
+  auc <- roc_curve$auc
+  
+  # Get the best threshold from the roc curve
+  # threshold <-  coords_best$threshold
+  
+  # Confusion Matrix
+  pred_class <- factor(ifelse(predicted >= threshold, 'Death', 'Alive'))
+  
+  label_fac <- factor(ifelse(label == 1, 'Death', 'Alive'))
+  
+  cm <- confusionMatrix(data = pred_class, reference = label_fac)
+  acc <- cm[['overall']]['Accuracy']
+  mcnemar <- cm[['overall']]['McnemarPValue']
+  sensitivity <- cm[['byClass']]['Sensitivity']
+  specificity <- cm[['byClass']]['Specificity']
+  ppv <- cm[['byClass']]['Pos Pred Value']
+  npv <- cm[['byClass']]['Neg Pred Value']
+  
+  # Hosmer Lemeshow Test 
+  # Define a small epsilon value
+  epsilon <- 1e-15
+  
+  # Adjust probabilities to avoid log(0)
+  adjusted_pred_risk <- pmax(pmin(predicted, 1 - epsilon), epsilon)
+  
+  # Calculate the Brier score manually
+  brier_score <- mean((label - adjusted_pred_risk)^2)
+  
+  # Calculate the logLoss manually
+  logloss <- -mean(label * log(adjusted_pred_risk) + (1 - label) * log(1 - adjusted_pred_risk))
+  
+  #### Hosmer Lemeshow Test ####
+  hlscore <- hoslem.test(label,predicted, g=10)
+  chisquare = hlscore$statistic
+  calib_pvalue = hlscore$p.value
+  brier = brier_score
+  logloss =logloss
+  
+  result <- data.frame(threshold, 
+                       auc, 
+                       acc, 
+                       sensitivity, 
+                       specificity, 
+                       ppv, 
+                       npv, 
+                       mcnemar, 
+                       chisquare, 
+                       calib_pvalue, 
+                       brier,
+                       logloss, 
+                       row.names = rowname )
+  return (result)
+}
+
+SearchBestThreshold <- function(label, predicted) {
+  label <- as.numeric(label) - 1
+  predicted <- as.numeric(predicted)
+  
+  best_threshold <- 0
+  best_f1 <- 0
+  
+  for (threshold in seq(0, 1, by = 0.001)) {
+    # Convert predicted probabilities to binary predictions using the threshold
+    pred_class <- factor(ifelse(predicted >= threshold, 'Death', 'Alive'),
+                         levels = c('Death', 'Alive'))
+    label_fac <- factor(ifelse(label == 1, 'Death', 'Alive'),
+                        levels = c('Death', 'Alive'))
+    
+    # Calculate precision, recall, and F1 score
+    
+    cm <- confusionMatrix(data = pred_class, reference = label_fac)
+    if (!is.na(cm[['byClass']]['Pos Pred Value']) && !is.na(cm[['byClass']]['Sensitivity'])) {
+      precision <- cm[['byClass']]['Pos Pred Value']
+      recall <- cm[['byClass']]['Sensitivity']
+      f1 <- 2 * (precision * recall) / (precision + recall)
+    
+      if (f1 > best_f1) {
+        best_f1 <- f1
+        best_threshold <- threshold
+        best_cm <- cm
+      }
+    }
+  }
+  
+  # Print the best F1 score and corresponding threshold
+  cat("Best F1 Score:", best_f1, "\n")
+  cat("Best Threshold:", best_threshold, "\n")
+  
+  return(best_threshold)
+}
