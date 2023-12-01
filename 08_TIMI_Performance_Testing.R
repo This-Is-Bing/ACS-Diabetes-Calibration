d22e6dd3d49061ae83ad93e86f3f96f2f9f9d239
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2023-12-01 12:00:58 +0800

    Adding Performance for TIMI

--- 08_TIMI_Performance_Testing.R
+++ 08_TIMI_Performance_Testing.R
@@ -2,8 +2,8 @@
 ####                        File Description & Setup                      ####
 #----------------------------------------------------------------------------#
 # Please run the rproj file in the folder                                     
-# Dataset: Diabetes ACS, STEMI, NSTEMI on Testing Dataset                                    
-# Description: To compare the raw model & calibrated model performance 
+# Dataset: Diabetes TIMI STEMI, NSTEMI on Testing Dataset                                    
+# Description: To compare the raw model & calibrated model performance on TIMI SCORE
 
 ##### Download eRic packages ####
 # UNCOMMENT TO INSTALL FOR THE FIRST TIME
@@ -24,7 +24,6 @@ library(ResourceSelection)
 library(rms)
 library(pROC)
 
-
 # ---------------------------------------------------------------------------#
 ####                       Loading and Transformation                     ####
 #----------------------------------------------------------------------------#
@@ -35,27 +34,45 @@ testing_ds <- read.csv('./dataset/processed/testing_ds.csv', header = TRUE)
 ##### Get all Features ####
 all_features <- read_xlsx('./dataset/NCVD_features_dm.xlsx')
 
-##### Structuring each feature ####
-testing_ds <- RF20_TransformationFunction(testing_ds, all_features)
-
 ##### Separating ACS, STEMI, NSTEMI
 stemi_testing_ds <- testing_ds[testing_ds$acsstratum ==1, ]
 nstemi_testing_ds <- testing_ds[!testing_ds$acsstratum ==1, ]
 
-##### Removing acsstratum, timiscorestemi, timiscorenstemi ####
-cols_to_remove <- c('acsstratum', 'timiscorestemi', 'timiscorenstemi')
-testing_ds <- testing_ds[, -which(names(testing_ds) %in% cols_to_remove)]
+##### Removing Uneccessary Columns for STEMI ####
+cols_to_remove <- c('acsstratum', 'ptoutcome', 'timiscorenstemi')
 stemi_testing_ds <- stemi_testing_ds[, -which(names(stemi_testing_ds) %in% cols_to_remove)]
+
+##### Removing Uneccessary Columns for SNTEMI ####
+cols_to_remove <- c('acsstratum', 'ptoutcome', 'timiscorestemi')
 nstemi_testing_ds <- nstemi_testing_ds[, -which(names(nstemi_testing_ds) %in% cols_to_remove)]
 
+##### Classifying outcome with timiscore fot STEMI ####
+stemi_testing_ds$timiscorestemi <- ifelse(stemi_testing_ds$timiscorestemi >=5, 1, 0)
+
+##### Classifying outcome with timiscore fot NSTEMI ####
+nstemi_testing_ds$timiscorenstemi <- ifelse(nstemi_testing_ds$timiscorenstemi >=5, 1, 0)
+
+##### Changing the outcome column name to make the code reusable ####
+stemi_testing_ds$ptoutcome <- stemi_testing_ds$timiscorestemi
+stemi_testing_ds$timiscorestemi<- NULL
+
+nstemi_testing_ds$ptoutcome <- nstemi_testing_ds$timiscorenstemi
+nstemi_testing_ds$timiscorenstemi<- NULL
+
+##### Structuring each feature ####
+stemi_testing_ds <- RF20_TransformationFunction(stemi_testing_ds, all_features)
+nstemi_testing_ds <- RF20_TransformationFunction(nstemi_testing_ds, all_features)
+
 ##### Separating features and label ####
-X_testing_ds <- testing_ds[,-which(names(testing_ds) == 'ptoutcome')]
+# X_testing_ds <- testing_ds[,-which(names(testing_ds) == 'ptoutcome')]
 X_stemi_testing_ds <- stemi_testing_ds[,-which(names(stemi_testing_ds) == 'ptoutcome')]
 X_nstemi_testing_ds <- nstemi_testing_ds[,-which(names(nstemi_testing_ds) == 'ptoutcome')]
 
-y_testing_ds <- testing_ds[,which(names(testing_ds) == 'ptoutcome')]
+# y_testing_ds <- testing_ds[,which(names(testing_ds) == 'ptoutcome')]
 y_stemi_testing_ds <- stemi_testing_ds[,which(names(stemi_testing_ds) == 'ptoutcome')]
 y_nstemi_testing_ds <- nstemi_testing_ds[,which(names(nstemi_testing_ds) == 'ptoutcome')]
+
+
 
 # ---------------------------------------------------------------------------#
 ####                        Predicting Probabilities                      ####
@@ -63,23 +80,23 @@ y_nstemi_testing_ds <- nstemi_testing_ds[,which(names(nstemi_testing_ds) == 'pto
 
 ##### Loading Models ####
 raw_model <- readRDS('./models/3_modelAll_rf20_dm.rds')$RF
-acs_calibrated_model <- readRDS('./results/calibrated_models/acs_calibrated_model.rds')
-stemi_calibrated_model <- readRDS('./results/calibrated_models/stemi_calibrated_model.rds')
-nstemi_calibrated_model <- readRDS('./results/calibrated_models/nstemi_calibrated_model.rds')
+# acs_calibrated_model <- readRDS('./results/calibrated_models/acs_calibrated_model.rds')
+timi_stemi_calibrated_model <- readRDS('./results/calibrated_models/timi_stemi_calibrated_model.rds')
+timi_nstemi_calibrated_model <- readRDS('./results/calibrated_models/timi_nstemi_calibrated_model.rds')
 
 ##### Prediction ####
 
 ###### ACS ####
-acs_raw_pred_probs <- predict(raw_model, X_testing_ds, type= 'prob')$Death
-acs_calibrated_pred_probs <- predict(acs_calibrated_model, data.frame(y = y_testing_ds, x = acs_raw_pred_probs), type = 'response')
+# acs_raw_pred_probs <- predict(raw_model, X_testing_ds, type= 'prob')$Death
+# acs_calibrated_pred_probs <- predict(acs_calibrated_model, data.frame(y = y_testing_ds, x = acs_raw_pred_probs), type = 'response')
 
 ###### STEMI ####
 stemi_raw_pred_probs <- predict(raw_model, X_stemi_testing_ds, type= 'prob')$Death
-stemi_calibrated_pred_probs <- predict(stemi_calibrated_model, data.frame(y = y_stemi_testing_ds, x = stemi_raw_pred_probs), type = 'response')
+stemi_calibrated_pred_probs <- predict(timi_stemi_calibrated_model, data.frame(y = y_stemi_testing_ds, x = stemi_raw_pred_probs), type = 'response')
 
 ###### NSTEMI ####
 nstemi_raw_pred_probs <- predict(raw_model, X_nstemi_testing_ds, type= 'prob')$Death
-nstemi_calibrated_pred_probs <- predict(nstemi_calibrated_model, data.frame(y = y_nstemi_testing_ds, x = nstemi_raw_pred_probs), type = 'response')
+nstemi_calibrated_pred_probs <- predict(timi_nstemi_calibrated_model, data.frame(y = y_nstemi_testing_ds, x = nstemi_raw_pred_probs), type = 'response')
 
 
 # ---------------------------------------------------------------------------#
@@ -89,32 +106,32 @@ nstemi_calibrated_pred_probs <- predict(nstemi_calibrated_model, data.frame(y = 
 
 ##### ACS ####
 ###### Raw Model Evaluation ####
-acs_raw_threshold <- SearchBestThreshold(y_testing_ds,acs_raw_pred_probs)
-acs_raw_test_result <- Evaluation(y_testing_ds, acs_raw_pred_probs, acs_raw_threshold, rowname = 'ACS_Raw_Test')
+# acs_raw_threshold <- SearchBestThreshold(y_testing_ds,acs_raw_pred_probs)
+# acs_raw_test_result <- Evaluation(y_testing_ds, acs_raw_pred_probs, acs_raw_threshold, rowname = 'ACS_Raw_Test')
 
 ###### Calibrated Model Evaluation ####
-acs_calibrated_threshold <- SearchBestThreshold(y_testing_ds,acs_calibrated_pred_probs)
-acs_calibrated_test_result <- Evaluation(y_testing_ds, acs_calibrated_pred_probs, acs_calibrated_threshold, rowname = 'ACS_Calibrated_Test')
+# acs_calibrated_threshold <- SearchBestThreshold(y_testing_ds,acs_calibrated_pred_probs)
+# acs_calibrated_test_result <- Evaluation(y_testing_ds, acs_calibrated_pred_probs, acs_calibrated_threshold, rowname = 'ACS_Calibrated_Test')
 
 
 ##### STEMI ####
 ###### Raw Model Evaluation ####
 stemi_raw_threshold <- SearchBestThreshold(y_stemi_testing_ds, stemi_raw_pred_probs)
-stemi_raw_test_result <- Evaluation(y_stemi_testing_ds, stemi_raw_pred_probs, stemi_raw_threshold, rowname = 'STEMI_Raw_Test')
+stemi_raw_test_result <- Evaluation(y_stemi_testing_ds, stemi_raw_pred_probs, stemi_raw_threshold, rowname = 'TIMI_STEMI_Raw_Test')
 
 ###### Calibrated Model Evaluation ####
 stemi_calibrated_threshold <- SearchBestThreshold(y_stemi_testing_ds,stemi_calibrated_pred_probs)
-stemi_calibrated_test_result <- Evaluation(y_stemi_testing_ds, stemi_calibrated_pred_probs, stemi_calibrated_threshold, rowname = 'STEMI_Calibrated_Test')
+stemi_calibrated_test_result <- Evaluation(y_stemi_testing_ds, stemi_calibrated_pred_probs, stemi_calibrated_threshold, rowname = 'TIMI_STEMI_Calibrated_Test')
 
 
 ##### NSTEMI ####
 ###### Raw Model Evaluation ####
 nstemi_raw_threshold <- SearchBestThreshold(y_nstemi_testing_ds, nstemi_raw_pred_probs)
-nstemi_raw_test_result <- Evaluation(y_nstemi_testing_ds, nstemi_raw_pred_probs, nstemi_raw_threshold, rowname = 'NSTEMI_Raw_Test')
+nstemi_raw_test_result <- Evaluation(y_nstemi_testing_ds, nstemi_raw_pred_probs, nstemi_raw_threshold, rowname = 'TIMI_NSTEMI_Raw_Test')
 
 ###### Calibrated Model Evaluation ####
 nstemi_calibrated_threshold <- SearchBestThreshold(y_nstemi_testing_ds,nstemi_calibrated_pred_probs)
-nstemi_calibrated_test_result <- Evaluation(y_nstemi_testing_ds, nstemi_calibrated_pred_probs, nstemi_calibrated_threshold, rowname = 'NSTEMI_Calibrated_Test')
+nstemi_calibrated_test_result <- Evaluation(y_nstemi_testing_ds, nstemi_calibrated_pred_probs, nstemi_calibrated_threshold, rowname = 'TIM_NSTEMI_Calibrated_Test')
 
 
 # ---------------------------------------------------------------------------#
@@ -122,11 +139,12 @@ nstemi_calibrated_test_result <- Evaluation(y_nstemi_testing_ds, nstemi_calibrat
 #----------------------------------------------------------------------------# 
 
 ##### Exporting Result ####
-final_result <- rbind(acs_raw_test_result,acs_calibrated_test_result,
-                      stemi_raw_test_result, stemi_calibrated_test_result,
+final_result <- rbind(stemi_raw_test_result, stemi_calibrated_test_result,
                       nstemi_raw_test_result, nstemi_calibrated_test_result)
 
-# write.csv(final_result, "./results/Performance_on_testing.csv")
+# write.csv(final_result, "./results/TIMI_Performance_on_testing.csv")
+
+
 
 
 
